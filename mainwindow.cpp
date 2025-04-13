bd285752523e8cf866102db6acb66dfba4678c45
fatpound <84704675+fatpound@users.noreply.github.com>
2025-04-13 13:46:40 +0300

    added CompileOptions + minor changes
    mainwindow.h -> mainwindow.hpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,9 +1,10 @@
-#include "mainwindow.h"
+#include "mainwindow.hpp"
 #include "./ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+    :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
 }
