e736b2c6ec2b09088b3910d3add373c6d19d5839
fatpound <84704675+fatpound@users.noreply.github.com>
2025-04-17 00:15:14 +0300

    Update CompileOptions.cmake
    removed unused & unnecessary features

--- CompileOptions.cmake
+++ CompileOptions.cmake
@@ -19,7 +19,7 @@ if (CMAKE_CXX_COMPILER_ID MATCHES "GNU")
 
 
         ## Preprocessor
-        -DFAT_BUILDING_WITH_MSVC=0
+        #
         
 
         ## Configuration-specific
@@ -60,7 +60,7 @@ elseif (CMAKE_CXX_COMPILER_ID MATCHES "Clang" OR CMAKE_CXX_COMPILER_ID MATCHES "
 
 
         ## Preprocessor
-        -DFAT_BUILDING_WITH_MSVC=0
+        #
 
 
         ## Configuration-specific
@@ -83,7 +83,6 @@ elseif (CMAKE_CXX_COMPILER_ID MATCHES "MSVC")
     target_compile_options (CompileOptions INTERFACE
         #####################################
         /permissive- # Conform to ISO/IEC C++
-        /std:c++latest
 
 
         ## Active warnings
@@ -101,7 +100,7 @@ elseif (CMAKE_CXX_COMPILER_ID MATCHES "MSVC")
         
 
         ## Preprocessor
-        -DFAT_BUILDING_WITH_MSVC=1
+        #
 
         
         ## Configuration-specific
@@ -115,9 +114,6 @@ elseif (CMAKE_CXX_COMPILER_ID MATCHES "MSVC")
 
             /GF # String pooling
             /GL # Whole-program optimization
-
-            /Qvec-report:2 # Auto vectorizer reporting (max)
-            /Qpar-report:2 # Parallelizer reporting (max)
         >
     )
 
