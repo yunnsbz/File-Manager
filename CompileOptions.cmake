d6e4af548e9994ef9a680ebef2f3b41fae1273f7
fatpound <84704675+fatpound@users.noreply.github.com>
2025-04-19 13:48:45 +0300

    updated CompileOptions

--- CompileOptions.cmake
+++ CompileOptions.cmake
@@ -1,12 +1,13 @@
 #[[ fatpound ]]#
 
 add_library (CompileOptions INTERFACE)
+add_library (Fat::CompileOptions ALIAS CompileOptions)
 
 if (CMAKE_CXX_COMPILER_ID MATCHES "GNU")
     target_compile_options(CompileOptions INTERFACE
         ##################################
-        -pedantic # Conform to ISO/IEC C++
-        
+        -pedantic # Conform to ISO/IEC Standard C++
+
 
         ## Active warnings
         -Wall
@@ -15,12 +16,14 @@ if (CMAKE_CXX_COMPILER_ID MATCHES "GNU")
 
 
         ## Inactive warnings
-        # -Wno-newline-eof
+        -Wno-newline-eof
 
 
         ## Preprocessor
-        #
-        
+        -DFAT_BUILDING_WITH_MSVC=0
+        $<$<STREQUAL:${CMAKE_HOST_SYSTEM_NAME},Linux>:   -DFAT_BUILDING_ON_WINDOWS=0>
+        $<$<STREQUAL:${CMAKE_HOST_SYSTEM_NAME},Windows>: -DFAT_BUILDING_ON_WINDOWS=1>
+
 
         ## Configuration-specific
         $<$<CONFIG:Debug>:
@@ -33,17 +36,15 @@ if (CMAKE_CXX_COMPILER_ID MATCHES "GNU")
         >
 
         ## Standard library
-        $<$<STREQUAL:${CMAKE_HOST_SYSTEM_NAME}, Linux>:
-            -stdlib=libstdc++
-        >
+        $<$<STREQUAL:${CMAKE_HOST_SYSTEM_NAME},Linux>: -stdlib=libstdc++>
     )
 
 elseif (CMAKE_CXX_COMPILER_ID MATCHES "Clang" OR CMAKE_CXX_COMPILER_ID MATCHES "IntelLLVM")
     target_compile_options (CompileOptions INTERFACE
         ##################################
-        -pedantic # Conform to ISO/IEC C++
+        -pedantic # Conform to ISO/IEC Standard C++
 
-        
+
         ## Active warnings
         -Weverything
 
@@ -51,22 +52,25 @@ elseif (CMAKE_CXX_COMPILER_ID MATCHES "Clang" OR CMAKE_CXX_COMPILER_ID MATCHES "
         ## Inactive warnings
         -Wno-c++98-compat
         -Wno-c++98-compat-pedantic
-        
+
         -Wno-c++20-compat
         -Wno-c++20-extensions
 
+        -Wno-missing-prototypes
+        -Wno-newline-eof
         -Wno-padded
-        # -Wno-newline-eof
 
 
         ## Preprocessor
-        #
+        -DFAT_BUILDING_WITH_MSVC=0
+        $<$<STREQUAL:${CMAKE_HOST_SYSTEM_NAME},Linux>:   -DFAT_BUILDING_ON_WINDOWS=0>
+        $<$<STREQUAL:${CMAKE_HOST_SYSTEM_NAME},Windows>: -DFAT_BUILDING_ON_WINDOWS=1>
 
 
         ## Configuration-specific
         $<$<CONFIG:Debug>:
             -O0
-            
+
             -Werror
         >
         $<$<CONFIG:Release>:
@@ -74,15 +78,29 @@ elseif (CMAKE_CXX_COMPILER_ID MATCHES "Clang" OR CMAKE_CXX_COMPILER_ID MATCHES "
         >
 
         ## Standard library
-        $<$<STREQUAL:${CMAKE_HOST_SYSTEM_NAME}, Linux>:
-            -stdlib=libc++
-        >
+        $<$<STREQUAL:${CMAKE_HOST_SYSTEM_NAME},Linux>: -stdlib=libc++>
     )
 
 elseif (CMAKE_CXX_COMPILER_ID MATCHES "MSVC")
     target_compile_options (CompileOptions INTERFACE
         #####################################
-        /permissive- # Conform to ISO/IEC C++
+        /permissive-        # Conform to ISO/IEC Standard C++
+        # /std:c++latest
+        # /std:clatest
+        # /nologo
+        # /MP               # Multi-processor compilation but it's not needed because CMake already launches multiple threads
+        /Zc:wchar_t         # treat 'wchar_t' as a built-in type
+        /Zc:preprocessor    # Use standards conforming preprocessor
+        /errorReport:none   # Don't report internal compiler errors
+
+
+        ## Code generation
+        /EHsc               # Enable C++ exceptions
+        /arch:AVX2          # Enable AVX2 extensions for code generation
+
+
+        ## Diagnostics
+        /diagnostics:column # Use old-style diagnostic messages because they are more readable when using MSVC
 
 
         ## Active warnings
@@ -90,6 +108,9 @@ elseif (CMAKE_CXX_COMPILER_ID MATCHES "MSVC")
 
 
         ## Inactive warnings
+        /external:anglebrackets # Treat headers included with <> as external
+        /external:W0            # Do NOT emit warnings for external headers
+
         /wd4061 # Not all enum identifiers of an Enum (class) are handled by a switch statement (When there is a default case)
         /wd4062 # Not all enum identifiers of an Enum (class) are handled by a switch statement (When there is NOT a default case)
         /wd4324 # structure was padded due to alignment specifier
@@ -97,23 +118,36 @@ elseif (CMAKE_CXX_COMPILER_ID MATCHES "MSVC")
         /wd4820 # n bytes padding added after construct MyClass
         /wd5045 # Compiler will insert Spectre mitigation for memory load if /Qspectre switch specified
         /wd5246 # outdated warning, the initialization of a subobject should be wrapped in braces
-        
 
-        ## Preprocessor
-        #
 
-        
+        ## Preprocessor definitions
+        /DFAT_BUILDING_WITH_MSVC=1
+        /DFAT_BUILDING_ON_WINDOWS=1
+        /DNOMINMAX
+        /DSTRICT
+        /D_UNICODE
+        /DUNICODE
+
+
         ## Configuration-specific
         $<$<CONFIG:Debug>:
             /Od
 
-            /WX # Treat warnings as errors
+            /WX  # Treat warnings as errors
+
+            /DIN_DEBUG=true
+            /DIN_RELEASE=false
         >
         $<$<CONFIG:Release>:
             /O2
 
-            /GF # String pooling
-            /GL # Whole-program optimization
+            /WX- # Do NOT Treat warnings as errors
+
+            /GF  # String pooling
+            /GL  # Whole-program optimization
+
+            /DIN_DEBUG=false
+            /DIN_RELEASE=true
         >
     )
 
